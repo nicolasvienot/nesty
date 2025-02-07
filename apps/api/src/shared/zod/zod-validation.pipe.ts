4ead3bc1b2ddd7c688bf21c535d4eafd15173411
nicolasvienot <nvienot@gmail.com>
2025-02-07 04:19:13 +0100

    fix ZodValidationPipe

--- apps/api/src/shared/zod/zod-validation.pipe.ts
+++ apps/api/src/shared/zod/zod-validation.pipe.ts
@@ -10,15 +10,17 @@ import { ZodSchema } from 'zod';
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema<any>) {}
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform<T>(value: any, metadata: ArgumentMetadata): T {
-    const validationResult = this.schema.safeParse(value);
-    if (!validationResult.success) {
-      throw new BadRequestException(
-        'Validation failed: \n' +
-          validationResult.error.errors.map((e) => e.message).join('\n'),
-      );
+    if (metadata.type === 'body') {
+      const validationResult = this.schema.safeParse(value);
+      if (!validationResult.success) {
+        throw new BadRequestException(
+          'Validation failed: \n' +
+            validationResult.error.errors.map((e) => e.message).join('\n'),
+        );
+      }
+      return validationResult.data as T;
     }
-    return validationResult.data as T;
+    return value as T;
   }
 }
