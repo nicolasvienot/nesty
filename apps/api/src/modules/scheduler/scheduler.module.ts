e10afd4d6623d2d1adc5f92005917d7e02f8f341
nicolasvienot <nvienot@gmail.com>
2025-02-14 17:08:41 +0100

    update scheduler

--- apps/api/src/modules/scheduler/scheduler.module.ts
+++ apps/api/src/modules/scheduler/scheduler.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { SchedulerService } from './scheduler.service';
-import { PrismaModule } from '@/shared/prisma/prisma.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), PrismaModule],
+  imports: [ScheduleModule.forRoot()],
   providers: [SchedulerService],
 })
 export class SchedulerModule {}
